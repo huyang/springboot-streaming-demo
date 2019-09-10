4c4a160fa30fc02e83d8b43ec46175b702525a51
Tiger Yang <yang@procor.com>
2019-09-10 15:03:37 -0400

    initial commit

---
+++ src/main/java/com/tigerits/example/demostreamingdata/DemoStreamingDataApplication.java
@@ -0,0 +1,13 @@
+package com.tigerits.example.demostreamingdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoStreamingDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoStreamingDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tigerits/example/demostreamingdata/DemoStreamingDataApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tigerits.example.demostreamingdata;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoStreamingDataApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
